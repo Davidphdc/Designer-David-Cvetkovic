5b4d1433331da03ccfe7c57dcc6a946640c6370f
David Cvetkovic <dc@davidcvetkovic.com>
2022-11-21 23:01:32 +1100

    dw

--- dist/static/js/views/Home.js
+++ dist/static/js/views/Home.js
@@ -11,8 +11,8 @@ export default class extends AbstractView {
     <div class="home">
     <div class="home_content">
       
-    <a href="#" class="logo"><img src="../static/assets/newlogodc01.png" alt=""></a>
-    <a href="#" class="biolink"> <h1>David Cvetkovic | Designer</h1></a>
+    <div class="logo"><img src="../static/assets/newlogodc01.png" alt=""></div>
+    <div class="biolink"><h1>David Cvetkovic | Designer</h1></div>
       
     </div>
     </div>
