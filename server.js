e78c0f489a153357bc0a0b10937d7b7106b0cd0a
David Cvetkovic <dc@davidcvetkovic.com>
2022-11-22 14:33:08 +1100

    dw

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const path = require("path");
+// const express = require("express");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "dist", "static")));
+// app.use("/static", express.static(path.resolve(__dirname, "dist", "static")));
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
-});
+// app.get("/*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+// });
 
-app.listen(process.env.PORT || 5500, () => console.log("Server running..."));
+// app.listen(process.env.PORT || 5500, () => console.log("Server running..."));
 
 
-const catalogRouter = require("./routes/catalog"); // Import routes for "catalog" area of site
-const compression = require("compression");
+// const catalogRouter = require("./routes/catalog"); // Import routes for "catalog" area of site
+// const compression = require("compression");
 
-// Create the Express application object
+// // Create the Express application object
 
-// …
+// // …
 
-app.use(compression()); // Compress all routes
+// app.use(compression()); // Compress all routes
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+// app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // …
