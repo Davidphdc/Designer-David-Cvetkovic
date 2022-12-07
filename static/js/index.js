d5d10cedc916897663cf846af660b131dc9d6455
David Cvetkovic <dc@davidcvetkovic.com>
2022-12-08 09:36:44 +1100

    dwdwd

--- static/js/index.js
+++ static/js/index.js
@@ -44,6 +44,6 @@ window.addEventListener("scroll", () => {
 
 // Recapcha
 
-// function onSubmit(token) {
-//   document.getElementById("cform").submit();
-// }
+var onloadCallback = function() {
+  alert("grecaptcha is ready!");
+};
