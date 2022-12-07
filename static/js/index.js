cb318f468e5c6bf4b3a5b4163f1e9014d6aab4c8
David Cvetkovic <dc@davidcvetkovic.com>
2022-12-07 16:25:26 +1100

    dw

--- static/js/index.js
+++ static/js/index.js
@@ -47,3 +47,13 @@ window.addEventListener("scroll", () => {
 function onSubmit(token) {
   document.getElementById("cform").submit();
 }
+
+
+function onClick(e) {
+  e.preventDefault();
+  grecaptcha.ready(function() {
+    grecaptcha.execute('reCAPTCHA_site_key', {action: 'submit'}).then(function(token) {
+        // Add your logic to submit to your backend server here.
+    });
+  });
+}
