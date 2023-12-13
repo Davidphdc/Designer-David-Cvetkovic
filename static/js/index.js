f20364b83023b39caebda2b6c7b53c6b1c5cb084
David Cvetkovic <dc@davidcvetkovic.com>
2023-12-13 16:07:52 +1100

    DC upload

--- static/js/index.js
+++ static/js/index.js
@@ -37,6 +37,33 @@ window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+// JavaScript
+// Get all the tabs and content divs
+var tabs = document.querySelectorAll(".tab");
+var contents = document.querySelectorAll(".content");
+
+// Loop through the tabs and add a click event listener to each one
+tabs.forEach(function(tab) {
+  tab.addEventListener("click", function(event) {
+    // Prevent the default action of the link
+    event.preventDefault();
+    // Get the content id from the data-content attribute of the tab
+    var contentId = tab.getAttribute("data-content");
+    // Get the content div that matches the content id
+    var content = document.getElementById(contentId);
+    // Remove the active class from all the tabs and content divs
+    tabs.forEach(function(tab) {
+      tab.classList.remove("active");
+    });
+    contents.forEach(function(content) {
+      content.classList.remove("active");
+    });
+    // Add the active class to the clicked tab and the corresponding content div
+    tab.classList.add("active");
+    content.classList.add("active");
+  });
+});
+
 
 
 
